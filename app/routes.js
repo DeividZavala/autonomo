e20cacfa896970b4d8bdc4e8d9f44b16dad7442d
Oswaldo <oswalfut_96@hotmail.com>
2016-10-07 17:51:13 -0500

    no

--- app/routes.js
+++ app/routes.js
@@ -15,12 +15,7 @@
             .when('/perfil/',{
                 template: `<perfil-component></perfil-component>`
                 })
-<<<<<<< HEAD
-
-            .when('/plazos/',{
-=======
             .when('/mensualidad/:id',{
->>>>>>> a4190162f2205c2f2f757f338f72a05a8c9ce0ba
                 template: `<car-component></car-component>`
                 })
 
