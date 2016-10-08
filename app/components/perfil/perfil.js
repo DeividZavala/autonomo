46d095f1cc3d098ab6e704315ab040daa091ada9
Héctor Feliciano Campos Muñoz <BlisS@MacBook-Pro-de-Hector.local>
2016-10-08 00:12:13 -0500

    perfil

--- app/components/perfil/perfil.js
+++ app/components/perfil/perfil.js
@@ -3,10 +3,11 @@
 		templateUrl:'app/components/perfil/index.html',
 		controller:perfilController
 	}
-	function perfilController($firebaseAuth,Api_service){
+	function perfilController($firebaseAuth,Api_service,$firebaseArray){
 		let self = this;
 
-		self.user = {}
+		self.user = {};
+		self.listaCoches = [];
 		self.auth = $firebaseAuth();
 
 		self.auth.$onAuthStateChanged(function(user) {
@@ -16,7 +17,28 @@
 			  	displayName:user.displayName,
 			  	email:user.email,
 			  	photoURL:user.photoURL
-		  }
+		  };
+
+		  	var ref = firebase.database().ref();
+    		var cocheraRef = ref.child("cochera");
+			var query = cocheraRef.orderByChild('user').equalTo(self.user.id);
+			console.log(query);
+			var list = $firebaseArray(query);
+			console.log(list);
+			list.$loaded()
+			    .then(function(){
+			        angular.forEach(list, function(lista) {
+			            console.log(lista);
+			           	self.listaCoches.push({
+			           		auto:lista.auto,
+			           		plazo:lista.plazo,
+			           		user:lista.user
+			           	}); 
+			        })
+			    });
+			
+
+
 		  } else {
 		    console.log("Signed out");
 		  }
@@ -24,7 +46,15 @@
 
 		Api_service.alls().then(function (response) {
 			self.userCars = response.data
-		})
+		});
+
+		
+			
+		
+		// Recupera sus coches
+		
+		// Recupera sus coches
+
 
 
 	}
