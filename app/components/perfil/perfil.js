9a5d8e886527da82829f99d6114ade8cf8cb7dcb
DeividZavala <crusedmoss@gmail.com>
2016-10-07 21:02:59 -0500

    user view first part

--- app/components/perfil/perfil.js
+++ app/components/perfil/perfil.js
@@ -3,8 +3,30 @@
 		templateUrl:'app/components/perfil/index.html',
 		controller:perfilController
 	}
-	function perfilController(){
+	function perfilController($firebaseAuth,Api_service){
 		let self = this;
+
+		self.user = {}
+		self.auth = $firebaseAuth();
+
+		self.auth.$onAuthStateChanged(function(user) {
+		  if (user) {
+		    self.user = {
+			  	id:user.uid,
+			  	displayName:user.displayName,
+			  	email:user.email,
+			  	photoURL:user.photoURL
+		  }
+		  } else {
+		    console.log("Signed out");
+		  }
+		});
+
+		Api_service.alls().then(function (response) {
+			self.userCars = response.data
+		})
+
+
 	}
 
 
